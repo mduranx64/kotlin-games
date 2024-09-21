cc0a00cc8d9d86b86b70937c62bed7e4fe0dd8c5
Miguel Duran <mduranr@falabella.cl>
2024-09-21 18:04:19 -0500

    Add custom splash screen

---
+++ KotlinGames/app/src/main/java/com/mduranx64/kotlingames/SplashActivity.kt
@@ -0,0 +1,63 @@
+package com.mduranx64.kotlingames
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.mduranx64.kotlingames.ui.theme.KotlinGamesTheme
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinGamesTheme {
+                SplashScreen()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreen() {
+    val alpha = remember { Animatable(0f) }
+    val context = LocalContext.current
+    LaunchedEffect(key1 = true) {
+        alpha.animateTo(
+            1f,
+            animationSpec = tween(1500)
+            )
+        val intent = Intent(context, MainActivity::class.java)
+        context.startActivity(intent)
+        (context as? Activity)?.finish()
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Image(
+            modifier = Modifier.alpha(alpha.value),
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = "Logo"
+        )
+    }
+}
+
