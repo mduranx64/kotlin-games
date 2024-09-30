5c9b85eef7cbe1b02a0d07cd875235caf43e810c
Miguel Duran <mduranx64@gmail.com>
2024-09-30 01:08:48 -0500

    Create android.yml

--- KotlinGames/app/build.gradle.kts
+++ KotlinGames/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = project.findProperty("app.id") as String?
         minSdk = 28
         targetSdk = 35
-        versionCode = 7
+        versionCode = 8
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
